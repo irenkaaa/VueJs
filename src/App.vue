968f41304c57bbe1509d5f5f7e8aeec57f7ce61b
Irena Mishonova <35763047+irenkaaa@users.noreply.github.com>
2021-04-01 11:44:54 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Navigation />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navigation from './components/core/Navigation';
+import Footer from './components/core/Footer';
 
 export default {
   name: 'App',
   components: {
     Navigation,
+    Footer
   }
 }
 </script>
---
+++ src/components/core/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>Irenkaaa Vue Project {{ date }}</footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: null,
+    };
+  },
+  created() {
+    console.log(new Date().getFullYear());
+    this.date = new Date().getFullYear();
+  },
+};
+</script>
+
+<style>
+footer {
+  border-top: 1px solid rgb(54, 48, 48);
+  margin-top: 0.5em;
+  text-align: center;
+  padding-top: 1em;
+}
+</style>
