1ef9ec63e98e8abcba11a6d46d411ce59c743485
Irena Mishonova <35763047+irenkaaa@users.noreply.github.com>
2021-04-01 10:53:22 +0200

    install/add Vuelidate & Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  VueRouter
 }).$mount('#app')
